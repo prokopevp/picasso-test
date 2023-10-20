02ea4e976c35cc4f436caefb71363d25085547d1
phdmabuze <pashokveok@gmail.com>
2023-10-20 20:04:16 +0300

    prelast

--- dj/files/tasks.py
+++ dj/files/tasks.py
@@ -16,25 +16,26 @@ def process_file(file_id):
     file = File.objects.get(id=file_id)
     file_path = file.file.path
     file_ext = os.path.splitext(file_path)[1]
+    try:
+        if file_ext in ['.jpg', '.png', '.bmp', '.gif', '.tif']:
+            # Блюр и чб изображения
+            with Image.open(file_path) as img:
+                img.load()
+            blured_img = img.filter(ImageFilter.BoxBlur(5))
+            gray_scaled = blured_img.convert("CMYK").convert("L")
+            gray_scaled.save(file_path)
+        if file_ext == '.pdf':
+            # Считывание текста в txt файл под именем filename.pdf.txt
 
-    if file_ext in ['.jpg', '.png', '.bmp', '.gif', '.tif']:
-        # Блюр и чб изображения
-        with Image.open(file_path) as img:
-            img.load()
-        blured_img = img.filter(ImageFilter.BoxBlur(5))
-        gray_scaled = blured_img.convert("CMYK").convert("L")
-        gray_scaled.save(file_path)
-
+            text = ''
+            with fitz.open(file_path) as doc:
+                for num, page in enumerate(doc.pages()):
+                    text += page.get_text()
+            text_file_path = os.path.join(MEDIA_ROOT, file.file.name+'.txt')
+            with open(text_file_path, "w", encoding='utf-8') as f:
+                f.write(text)
+    except Exception as e:
+        print(e)
+    finally:
         file.processed = True
         file.save()
-
-    if file_ext == '.pdf':
-        # Считывание текста в txt файл под именем filename.pdf.txt
-
-        text = ''
-        with fitz.open(file_path) as doc:
-            for num, page in enumerate(doc.pages()):
-                text += page.get_text()
-        text_file_path = os.path.join(MEDIA_ROOT, file.file.name+'.txt')
-        with open(text_file_path, "w", encoding='utf-8') as f:
-            f.write(text)
